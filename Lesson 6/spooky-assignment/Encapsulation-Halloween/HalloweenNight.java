08764db9d399ce91ad2b5b14306e0c57cbccbb1e
snorklar <shawnrehan27@gmail.com>
2025-11-23 17:26:16 -0500

    Update HalloweenNight.java

--- Lesson 6/spooky-assignment/Encapsulation-Halloween/HalloweenNight.java
+++ Lesson 6/spooky-assignment/Encapsulation-Halloween/HalloweenNight.java
@@ -1,8 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
 public class HalloweenNight {
     
     public static void main(String[] Args){
     
         TrickOrTreater spider = new TrickOrTreater("peter the spider");
+        System.out.println(spider.getName());
+        spider.collectCandy("Reece's");
+        spider.getGandy();
+        spider.collectCandy("Reece's");
+        spider.getGandy(); spider.collectCandy("Reece's");
+        spider.getGandy(); spider.collectCandy("Reece's");
+        spider.getGandy(); spider.collectCandy("Reece's");
+        spider.collectCandy("Reece's");
+        spider.getGandy();
+        spider.eatCandy("Reece's");
+        spider.eatCandy("Reece's");
+        spider.eatCandy("Reece's");
+        spider.eatCandy("Reece's");
+        spider.eatCandy("Reece's");
+        spider.eatCandy("Reece's");
+        spider.showStatus();
         /* (Class)    (label) = new Constructor(parameters);
         * First java will allocate space in memory for a TrickOrTreater Object.
         * It will then look into the constructor function:
@@ -12,4 +35,3 @@ public class HalloweenNight {
 
     }
   }
-
